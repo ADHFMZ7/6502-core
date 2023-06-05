e97c3dc4e5a1852ef186ee73bd912c3e36bd951c
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-06-04 23:22:33 -0700

    start MMU

--- 6502/cpu.py
+++ 6502/cpu.py
@@ -1,63 +1,74 @@
 from enum import Enum
 
-class OP(Enum):
+MEM_SIZE = 2 ** 16 
 
-  ADC = 0b1000101
-  AND = 0b1000001
-  ASL = 
-  BCC
-  BCS
-  BEQ
-  BIT
-  BMI
-  BNE
-  BPL
-  BRK
-  BVC
-  BVS
-  CLC
-  CLD
-  CLI
-  CLV
-  CMP
-  CPX
-  CPY
-  DEC
-  DEX
-  DEY
-  EOR
-  INC
-  INX
-  INY
-  JMP
-  JSR
-  LDA
-  LDX
-  LDY
-  LSR
-  NOP
-  ORA
-  PHA
-  PHP
-  PLA
-  PLP
-  ROL
-  ROR
-  RTI
-  RTS
-  SBC
-  SEC
-  SED
-  SEI
-  STA
-  STX
-  STY
-  TAX
-  TAY
-  TSX
-  TXA
-  TXS
-  TYA
+class Memory:
+
+  def __init__(self):
+    
+
+def execute(address, data, mode):
+  
 
 
+
+
+# class OP(Enum):
+#
+#   ADC = 0b01000101
+#   AND = 0b01000001
+#   ASL = 
+#   BCC
+#   BCS
+#   BEQ
+#   BIT
+#   BMI
+#   BNE
+#   BPL
+#   BRK
+#   BVC
+#   BVS
+#   CLC
+#   CLD
+#   CLI
+#   CLV
+#   CMP
+#   CPX
+#   CPY
+#   DEC
+#   DEX
+#   DEY
+#   EOR
+#   INC
+#   INX
+#   INY
+#   JMP
+#   JSR
+#   LDA
+#   LDX
+#   LDY
+#   LSR
+#   NOP
+#   ORA
+#   PHA
+#   PHP
+#   PLA
+#   PLP
+#   ROL
+#   ROR
+#   RTI
+#   RTS
+#   SBC
+#   SEC
+#   SED
+#   SEI
+#   STA
+#   STX
+#   STY
+#   TAX
+#   TAY
+#   TSX
+#   TXA
+#   TXS
+#   TYA
 
---
+++ 6502/mmu.py
@@ -0,0 +1,8 @@
+
+MEM_SIZE = 2 ** 16
+
+class MMU:
+
+  
+
+
