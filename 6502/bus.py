43a7957e575415109d175b23d44d0cf638ad783d
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-06-06 08:12:26 -0700

    add bus functionality

--- 6502/bus.py
+++ 6502/bus.py
@@ -4,7 +4,15 @@
 
 class BUS:
 
-
-
-    def __init__(Self):
-        self.data = 
+    def __init__(self):
+        
+        self.memory = [0x0] * 64 * 1024
+        
+        self.data = 0x0
+        self.address = 0x0
+        
+    def read(self, address):
+        return self.memory[address]
+    
+    def write(self, address, data):
+        self.memory[address] = data
